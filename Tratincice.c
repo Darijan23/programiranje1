42d699910e61681210ba177ae870ee7c1cac5624
Darijan Jelušić <darijan.jelusic@gmail.com>
2020-01-18 16:58:38 +0100

    Update

--- Tratincice.c
+++ Tratincice.c
@@ -20,11 +20,13 @@ int main()
 
 	for (i = 0; i < n; i++) {
 		for (j = 0; j < m; j++) {
+			//ova dva fora prolaze kroz polje i traže 'S'
 			if (vrt[i][j] == 'S') {
-				for (k = -1; k < 2; k++) {
-					for (l = -1; l < 2; l++) {
-						if (i+k < n && i+k >= 0 && j+l < m && j+l >= 0) {
-							if (vrt[i+k][j+l] != 'S') {
+				for (k = -1; k <= 1; k++) {
+					for (l = -1; l <= 1; l++) {
+						//nakon što nađe 'S', vrti od -1 do 1 po x i y (k i l) i to dodaje indeksu na kojem se nalazi 'S'
+						if (i+k < n && i+k >= 0 && j+l < m && j+l >= 0) {   	//ovim ifom izbjegne granične slučajeve
+							if (vrt[i+k][j+l] != 'S') { 						//ovim ifom ignoriramo stablo
 								vrt[i+k][j+l] = 'T';
 							}
 						}
