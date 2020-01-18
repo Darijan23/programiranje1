8a2178208971b5a5d25943ca14cc5923f74951a0
Darijan Jelušić <darijan.jelusic@gmail.com>
2020-01-18 17:52:48 +0100

    Update

--- Poker.c
+++ Poker.c
@@ -16,13 +16,30 @@ typedef struct {
 } player;
 
 int main() {
-	int i, n;
+	int i;
+	int par[] = {0, 0, 0, 0, 0, 0, 0};
 	player table[7];
-	scanf("%d\n", &n);
 
-	for (i = 0; i < n; ++i)	{
+	for (i = 0; i < 3; ++i)	{
 		scanf("%d\n%c %c\n%c %c ", &table[i].pos, &table[i].c1.col, &table[i].c1.val, &table[i].c2.col, &table[i].c2.val);
+		if (table[i].c1.col == table[i].c2.col) {
+			printf("Igrac na poziciji %d ima mogucnost: boja\n", table[i].pos);
+		} else if (table[i].c1.val == table[i].c2.val) {
+			printf("Igrac na poziciji %d ima mogucnost: par\n", table[i].pos);
+		} else {
+			printf("Igrac na poziciji %d ima mogucnost: nista\n", table[i].pos);
+		}
+
+		if (table[i].c1.val == table[i].c2.val) {
+			par[i] = table[i].pos;
+		}
 	}
 
+	printf("Par imaju igraci:");
+	for (i = 0; i < 3; ++i) {
+		if (par[i]) {
+			printf(" %d", par[i]);
+		}
+	}
 	return 0;
 }
