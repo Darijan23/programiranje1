9a369b2e28c3b414960859e2d3cb95cfc082ebd1
Darijan Jelušić <darijan.jelusic@gmail.com>
2020-01-18 14:26:08 +0100

    Update

--- Kvadrat_ili_ne.c
+++ Kvadrat_ili_ne.c
@@ -1,3 +1,5 @@
+//Ne prolaze dva test primjera
+
 #include <stdio.h>
 #include <math.h>
 
@@ -5,21 +7,42 @@
 #define DEBUG(...)printf(__VA_ARGS__)
 #endif
 
-struct point{
+typedef struct{
 	int x;
 	int y;
-};
+} point;
 
-float dist(struct point p1, struct point p2) {
+float dist(point p1, point p2) {
 	return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
 }
 
 int main() {
-	int i;
-	struct point pts[4];
+	int i, indiag; //indiag je indeks udaljenosti dijagonale
+	float lens[3], diag = 0.0, sum = 0.0;
+	point pts[4];
 
 	for (i = 0; i < 4; i++) {
 		scanf("%d %d\n", &pts[i].x, &pts[i].y);
+	}
+
+	for (i = 0; i < 3; i++) {
+		lens[i] = dist(pts[0], pts[i]);
+		if (dist(pts[0], pts[i]) > diag) {
+			diag = dist(pts[0], pts[i]);
+			indiag = i;
+		}
+	}
+
+	for (i = 0; i < 3; i++) {
+		if (i != indiag) {
+			sum += pow(lens[i], 2);
+		}
+	}
+
+	if (round(pow(diag, 2)) == round(sum)) {
+		printf("Kombiniram kvadrat");
+	} else {
+		printf("Ne kombiniram");
 	}
 
 
